190472ebcb0da536b85594ae492ca1388ac2deb3
mbmcauliffe <85567376+mbmcauliffe@users.noreply.github.com>
2022-09-10 13:49:55 -0400

    Initial Upload

---
+++ js/global.js
@@ -0,0 +1,78 @@
+function raiseError(error){
+	document.getElementById("errorTitle").innerHTML = error.title;
+	document.getElementById("errorMessage").innerHTML = error.message;
+	document.getElementById("errorBlock").style.setProperty("display", "block");
+}
+
+function dismissError(){
+	document.getElementById("errorBlock").style.setProperty("display", "none");
+}
+
+async function logout(){
+
+	const response = await fetch('/logout', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+
+  if(response.status === 200){
+
+  	window.location.replace("/login");
+
+  }
+
+}
+
+function enterHandling(keyPress, callback=null){
+
+  if(keyPress == 'Enter'){
+
+    if(callback == null){
+
+      const inputs = Array.from(document.getElementsByTagName("input"));
+
+      const currentIndex = inputs.indexOf(document.activeElement);
+
+      inputs[currentIndex + 1].focus();
+
+    } else {
+
+      callback();
+
+    }
+
+    return keyPress != 'Enter';
+
+  }
+
+  return
+  
+}
+
+function raisePrompt(){
+
+  document.getElementById("promptBox").style.setProperty("display", "block");
+
+}
+
+function closePrompt(clear=false){
+
+  const promptBox = document.getElementById("promptBox");
+
+  promptBox.style.setProperty("display", "none");
+
+  if(clear == true){
+
+    const promptInputs = Array.from(promptBox.getElementsByTagName("input"));
+
+    for(i=0;i<promptInputs.length;i++){
+
+      promptInputs[i].value = "";
+
+    }
+
+  }
+
+}
